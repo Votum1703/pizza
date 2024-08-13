0d09f5739dc6aea16e7fcc7ad636a358e85e1153
Shakes Peare <votum1703@gmail.com>
2024-08-13 21:51:31 +0300

    pizza #7

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,15 +9,17 @@ import PizzaBlock from '../components/PizzaBlock';
 import Skeleton from '../components/PizzaBlock/Skeleton';
 import Pagination from '../components/Pagination';
 import { SearchContext } from '../App';
+import { setItems } from '../redux/slices/pizzaSlice';
 
 
   const Home = () => {
   const dispatch = useDispatch( )
+  const isSearch = React.useRef(false);
+  const items = useSelector(state => state.pizza.items)
   const {categoryId, sort, currentPage} = useSelector(state => state.filter)
 
 
   const {searchValue} = React.useContext(SearchContext)
-  const [items, setItems] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(true);
 
   const onChangeCategory = (id) => {
@@ -29,25 +31,44 @@ import { SearchContext } from '../App';
     dispatch(setCurrentPage(number))
   }
 
-  React.useEffect(() => {
+  const fetchPizzas = async () => {
     setIsLoading(true);
 
+    
     const sortBy = sort.sortProperty.replace('-', '')
     const order = sort.sortProperty.includes('-') ? 'asc' : 'desc'
     const category = categoryId > 0 ? `category=${categoryId}` : ''
     const search = searchValue ? `&search=${searchValue}` : ''
 
 
-      axios.get(
-        `https://65d63e23f6967ba8e3bdc5bc.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}`
-      )
-      .then(res => {
-        setItems(res.data);
-        setIsLoading(false);
-      })
 
 
-      window.scrollTo(0, 0)
+try {
+  const {data} = await axios.get(
+    `https://65d63e23f6967ba8e3bdc5bc.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}`)
+  dispatch(setItems(data))
+} catch (error) {
+    setIsLoading(false);
+    console.log('ERROR', error);
+    alert('Ошибка загрузки данных')
+
+} finally {
+  setIsLoading(false);
+
+}
+
+     window.scrollTo(0, 0);
+}
+
+
+  React.useEffect(() => {
+   
+
+    if (!isSearch.current) {
+      fetchPizzas();
+    }
+
+    isSearch.current = false;
   }, [categoryId, sort.sortProperty, searchValue, currentPage]);
 
   const pizzas = items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)
